39172c83c73bac2f1294ed9674e43eacdce5d6a2
jencaro <jencaro19@gmail.com>
2022-05-25 14:20:01 +0200

    cleaning refactoring script

--- spicy_snake/snake.py
+++ spicy_snake/snake.py
@@ -1,8 +1,11 @@
 
+from multiprocessing.spawn import prepare
+from folium import LayerControl
 from spicy_snake.playground import Playground
+from spicy_snake.screen_helper import prepare_screen
 import curses
 
-
+# definition of constants UPPERCASE
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 UP = (0, -1)
@@ -10,60 +13,50 @@ DOWN = (0, 1)
 
 # ASCII codes of characters on the keyboards
 KEY_COMMANDS = {97: LEFT, 100: RIGHT, 119: UP, 115: DOWN}
-#TODO: use arrow keys instead
+#KEY_COMMANDS = {27: LEFT, 26: RIGHT, 24:UP, 25: DOWN} #FIXME:windows arrow keys are not working
 
-# prepare the screen
-screen = curses.initscr()
-curses.start_color()
-curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
-curses.curs_set(0)
-curses.noecho()
-curses.raw()
-screen.keypad(False)
-
-win = curses.newwin(40, 15, 0, 0)
-win.nodelay(True)
+# symbols
+SNAKE_SYMBOL = "0"
+WALL_SYMBOL = "#"
 
 
-def game_loop(screen):
-    x, y = 5, 5 # player position
-
-    pg = Playground(30, 14)
-
-    # draw
-    screen.clear() #FIXME:redundance from line 52-59
+def draw(player_pos, pg): # we are not creating any new variables or information so we do not need to call the function
+    screen.clear() 
     # draw the player
-    screen.addch(y, x, "O", curses.color_pair(1))
+    #TODO: we could seperate the draw and draw_player into seperate function
+    x, y = player_pos
+    screen.addch(y, x, SNAKE_SYMBOL, curses.color_pair(1)) # making the symbol a variable
     # draw playground
-    for pgx in range(31):
-        for pgy in range(15):
+    for pgx in range(pg.xsize + 1):
+        for pgy in range(pg.ysize + 1):
             if pg.is_obstacle((pgx, pgy)):
-                screen.addch(pgy, pgx, "#", curses.color_pair(2))
+                screen.addch(pgy, pgx, WALL_SYMBOL, curses.color_pair(2))
     win.refresh()
     screen.refresh()
+
+win, screen = prepare_screen()
+
+def move_player(player_position, direction):
+    dx, dy = direction
+    x, y = player_position
+    x += dx
+    y += dy
+    return x, y
+
+def game_loop(screen):
+    player_position = 5, 5 # player position
+    pg = Playground(30, 14)
+    draw(player_position, pg, win, screen)
 
     while True:
         # move player around
         char = win.getch()
-        direction = KEY_COMMANDS.get(char) # direction is a tuple or None
+        direction = KEY_COMMANDS.get(char) # direction is a tuple or None # returns the code of a pressed key
         if direction:
-            dx, dy = direction
-            x += dx
-            y += dy
-            #FIXME: name collision x and y
-
+            player_position = move_player(player_position, direction)
 
             # draw
-            screen.clear() #FIXME:the order of these codes are different fromthe other one
-            screen.addch(y, x, "O", curses.color_pair(1))
-            for pgx in range(31):
-                for pgy in range(15):
-                    if pg.is_obstacle((pgx, pgy)):
-                        screen.addch(pgy, pgx, "#", curses.color_pair(2))
-            win.refresh() # FIXED: the order improved the code the wall does not disappear
-            screen.refresh()
-
+            draw(player_position, pg, win, screen) # call back the draw screen function 
 
 if __name__ == "__main__":
 
